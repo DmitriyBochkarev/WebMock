a1dbe62dbab990fb0135695132200c790a5848d7
bochkarevda <bochkarevda@ekassir.com>
2025-06-07 00:13:11 +0300

    modalCreate

---
+++ WebApplication1/Scripts/modalcreate.js
@@ -0,0 +1,8 @@
+﻿// Функция открытия модального окна для редактирования
+function openCreateModal() {
+    const modal = document.getElementById('mockCreateModal');
+    
+            // Открываем модальное окно
+            modal.style.display = 'block';
+     
+}
